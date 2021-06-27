6a6dd6d5962115005c07f4aaf9f00c9e0789e623
anshika1709 <awasthi.anshika1709@gmail.com>
2021-06-27 21:43:53 +0530

    Build a movie row

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React from "react";
 import Row from "./Row";
+import "./App.css";
 import requests from "./request";
 const App = () => {
   return (
     <div>
       <Row title="NETFLIX ORIGINALS" fetchUrl={requests.fetchNetflixOriginals} />
       <Row title="TRENDING NOW" fetchUrl={requests.fetchTrending} />
+      <Row title="TOP RATED" fetchUrl={requests.fetchTopRated} />
+      <Row title="ACTION MOVIES" fetchUrl={requests.fetchActionMovies} />
+      <Row title="HORROR MOVIES" fetchUrl={requests.fetchHorrorMovies} />
+      <Row title="ROMANCE ARIES" fetchUrl={requests.fetchRomanceMovies} />
+      <Row title="DOCUMENT ARIES" fetchUrl={requests.fetchDocumentaries} />
     </div>
   );
 };
--- src/Row.js
+++ src/Row.js
@@ -1,22 +1,31 @@
 import React, { useEffect, useState } from "react";
 import axios from "./axios";
+import "./Row.css";
 
 function Row({ title, fetchUrl }) {
   const [movies, setMovies] = useState([]);
-
+  const baseURL = "https://image.tmdb.org/t/p/original";
   useEffect(() => {
     //Asyc fxn because we cant make a hook Async function
     async function fetchData() {
       //fetchUrl will be mentioned in the array
       const request = await axios.get(fetchUrl);
+      setMovies(request.data.results);
+
       return request;
     }
     fetchData();
   }, [fetchUrl]);
 
+  console.table(movies);
   return (
-    <div>
+    <div className="row">
       <h1>{title}</h1>
+      <div className="row_posters">
+        {movies.map((movie) => {
+          return <img key={movie.id} className="row_poster" src={`${baseURL}${movie.poster_path}`} alt={movie.name} />;
+        })}
+      </div>
     </div>
   );
 }
