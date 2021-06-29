99791f663cf90573f833fc8e5043f5f56c39bdb5
anshika1709 <awasthi.anshika1709@gmail.com>
2021-06-29 16:34:38 +0530

    Add Banner

--- src/App.js
+++ src/App.js
@@ -2,16 +2,19 @@ import React from "react";
 import Row from "./Row";
 import "./App.css";
 import requests from "./request";
+import Banner from "./Banner";
+
 const App = () => {
   return (
-    <div>
+    <div className="app">
+      <Banner />
       <Row title="NETFLIX ORIGINALS" fetchUrl={requests.fetchNetflixOriginals} isLargeRow />
-      <Row title="TRENDING NOW" fetchUrl={requests.fetchTrending} />
-      <Row title="TOP RATED" fetchUrl={requests.fetchTopRated} />
-      <Row title="ACTION MOVIES" fetchUrl={requests.fetchActionMovies} />
-      <Row title="HORROR MOVIES" fetchUrl={requests.fetchHorrorMovies} />
-      <Row title="ROMANCE ARIES" fetchUrl={requests.fetchRomanceMovies} />
-      <Row title="DOCUMENT ARIES" fetchUrl={requests.fetchDocumentaries} />
+      <Row title="Trending Now" fetchUrl={requests.fetchTrending} />
+      <Row title="Top Rated" fetchUrl={requests.fetchTopRated} />
+      <Row title="Action Movies" fetchUrl={requests.fetchActionMovies} />
+      <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies} />
+      <Row title="Romance Aries" fetchUrl={requests.fetchRomanceMovies} />
+      <Row title="Document Aries" fetchUrl={requests.fetchDocumentaries} />
     </div>
   );
 };
